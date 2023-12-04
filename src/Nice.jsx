2c3b45e7d73012133bc8bf8460fd8a7ce50408a5
Adamskid9 <ajaniadam9@gmail.com>
2023-12-04 23:16:54 +0100

    one

--- src/Nice.jsx
+++ src/Nice.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-function Test() {
+function Nice() {
     return (
         <div>
             hello world <br/>
@@ -9,7 +9,7 @@ function Test() {
     )
 }
 
-export default Test
+export default Nice
 
 //   git remote add origin https://github.com/Adamskid9/testing-router.git
 //   git push -u origin main
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Test from './test.jsx'
+import Nice from './Nice.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<App/>}/>
-    <Route path='test' element={<Test/>}/>
+    <Route path='test' element={<Nice/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
