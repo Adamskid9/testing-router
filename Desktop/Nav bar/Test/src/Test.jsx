7eb108e9c05e01719068981876f15f8263da0557
Adamskid9 <ajaniadam9@gmail.com>
2023-12-04 22:58:24 +0100

    one

---
+++ Desktop/Nav bar/Test/src/Test.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Test() {
+    return (
+        <div>
+            hello world <br/>
+            <Link to={"/"}>main page</Link>
+        </div>
+    )
+}
+
+export default Test
